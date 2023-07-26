84048b8b047a4d58b91a9a781e41287c385047af
yesware <meoyes9@gmail.com>
2023-07-26 05:14:05 +0100

    full signup

--- register.php
+++ register.php
@@ -14,32 +14,33 @@
     <div class="content"> 
 
      <h2>Sign Up</h2> 
+     <form method="post"> 
 
      <div class="form"> 
 
       <div class="inputBox"> 
 
-       <input type="text" required> <i>FamilyName</i> 
+       <input type="text" name="fName" required> <i>FamilyName</i> 
 
       </div> 
 
       <div class="inputBox"> 
 
-       <input type="text" required> <i>Name</i> 
+       <input type="text" name="name" required> <i>Name</i> 
 
       </div> 
       <div class="inputBox"> 
 
-       <input type="email" required> <i>E-mail</i> 
+       <input type="email" name="email" required> <i>E-mail</i> 
 
       </div> 
       <div class="inputBox"> 
 
-       <input type="password" required> <i>password</i>
+       <input type="password" name="password" required> <i>password</i>
 
       <div class="inputBox"> 
       <br>
-      <input type="submit" value="Sign Up">
+      <input type="submit" name="signup" value="Sign Up">
 
       </div> 
       
@@ -49,9 +50,32 @@
     </div> 
 
    </div> 
+</form>
 
   </section> <!-- partial --> 
 
  </body>
 
 </html>
+
+<?php
+include 'db.php';
+
+$fam_name = $_POST['fName'];
+$nom = $_POST['name'];
+$e_mail = $_POST['email'];
+$passwrd = $_POST['password'];
+
+if(!empty($_POST['signup'])){
+
+if(!empty($fam_name) && !empty($nom) && !empty($e_mail) && !empty($passwrd)){
+    $sql = "INSERT INTO user (Fname, name, email, password) VALUES (?,?,?,?)";
+    $sql1 = $db->prepare($sql);
+    $exec2 = $sql1->execute([$fam_name, $nom, $e_mail, sha1($passwrd)]);
+}
+}
+
+
+
+
+?>
